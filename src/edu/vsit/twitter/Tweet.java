42f763531e2e5ed7d5185c6616ee3e9952f2138d
Sahil Makhijani <sahil.makhijani@gmail.com>
2022-12-05 21:00:33 +0530

    Created Tweet Class

---
+++ src/edu/vsit/twitter/Tweet.java
@@ -0,0 +1,12 @@
+package edu.vsit.twitter;
+
+import java.util.Date;
+
+public class Tweet {
+    private int id;
+    private String user;
+    private String content;
+    private Date timestamp;
+
+
+}
