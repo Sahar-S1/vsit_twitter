4b0655fb30ace219bcfac911eb3de068e548c8d4
Sahil Makhijani <sahil.makhijani@gmail.com>
2022-12-05 21:48:44 +0530

    Added constructor, getters, setters and toString to Tweet class

--- src/edu/vsit/twitter/Driver.java
+++ src/edu/vsit/twitter/Driver.java
@@ -2,6 +2,19 @@ package edu.vsit.twitter;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Tweet myfirsttweet = new Tweet();
+        System.out.println(myfirsttweet);
+
+        Tweet t2 = new Tweet();
+        System.out.println(t2);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("ERROR");
+        }
+
+        t2.setContent("Hello World");
+        System.out.println(t2);
     }
 }
--- src/edu/vsit/twitter/Tweet.java
+++ src/edu/vsit/twitter/Tweet.java
@@ -3,10 +3,61 @@ package edu.vsit.twitter;
 import java.util.Date;
 
 public class Tweet {
+    // Properties
     private int id;
     private String user;
     private String content;
     private Date timestamp;
 
+    // Default Constructor
+    public Tweet() {
+        System.out.println("Tweet() called");
+        this.id = 0;
+        this.user = "<Anonymous>";
+        this.content = "...";
+        this.timestamp = new Date();
+    }
 
+    // Getter Setter Methods
+    public int getId() {
+        return id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+        updateTimestamp();
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    // Other Methods
+
+    private void updateTimestamp() {
+        this.timestamp = new Date();
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "Tweet{" +
+                "id=" + id +
+                ",user=" + user +
+                ",content=" + content +
+                ",timestamp=" + timestamp +
+                '}';
+    }
 }
